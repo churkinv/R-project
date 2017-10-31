d70ffef1c491e9f1a7c1818297b007139380937b
churkinv <churkinv@gmail.com>
2017-10-31 23:49:01 +0200

    descriptive statics script created

---
+++ DataScienceR/DescriptiveStatistics.R
@@ -0,0 +1,10 @@
+#set the working directory
+setwd("E:/OneDrive/Проекты/IT_Step/VS_2017/DataScienceR/DataScienceR")
+
+#read a CSV data file
+cars <- read.csv("Cars.csv")
+
+#peek at the date
+head(cars)
+
+#create a freaquency table
