15a94d797c28f9138c76ff83bde034400bcacdd4
churkinv <churkinv@gmail.com>
2017-11-03 22:15:56 +0200

    Linear regression on big data

--- DataScienceR/BigData.R
+++ DataScienceR/BigData.R
@@ -17,7 +17,34 @@ names(irisff)
 irisff[1:5,]
 
 #load the biglm package
-install.packages("biglm")
+install.packages("biglm") # special package for linear regression with big data which use much less memory than traditional l.regression algorithm
 library(biglm)
 
 #create a linear regression model
+model <- biglm(
+               formula = Petal.Width ~ Petal.Length,
+               data = irisff)
+
+#summaraize the model
+summary(model)
+
+#create a scatterplot
+plot(
+     x = irisff$Petal.Length[],
+     y = irisff$Petal.Width[],
+     main = "Iris Petal Legth vs. Width",
+     xlab = "Petal Length(cm)",
+     ylab = "Petal Width(cm)")
+
+#get y-intercept from the model
+b <- summary(model)$mat[1, 1]
+
+#get slope from the model
+m <- summary(model)$mat[2, 1]
+
+#draw a regression line on plot
+lines(
+      x = irisff$Petal.Length[],
+      y = m * irisff$Petal.Length[] + b,
+      col = "red",
+      lwd = 3)
