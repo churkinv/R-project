1610f053463f6aac49ec8f98365a953fee86378f
churkinv <churkinv@gmail.com>
2017-11-03 21:52:19 +0200

    linear regression model created

---
+++ DataScienceR/StatisticalModels.R
@@ -0,0 +1,29 @@
+
+#load the date
+data(iris)
+
+#peak at data
+head(iris)
+
+#create a scatterplot
+plot(
+     x = iris$Petal.Length,
+     y = iris$Petal.Width,
+     main = "Iris Peral Length vs. Width",
+     xlab = "Petal Length (cm)",
+     ylab = "Petal Width(cm)")
+
+#create a linear regression model
+model <- lm(
+            formula = Petal.Width ~ Petal.Length,
+            data = iris)
+
+#summarize the model
+summary(model)
+
+#draw a regression line on plot
+lines(
+      x = iris$Petal.Length,
+      y = model$fitted,
+      col = "red",
+      lwd = 3)
