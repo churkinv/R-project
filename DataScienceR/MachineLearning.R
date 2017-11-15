02ec3c82a6df5dc9205a1a3c1a940b9075275f65
churkinv <churkinv@gmail.com>
2017-11-15 04:38:51 +0200

    loaded a color brewer library, created a scatterplot colored by species, plotted the decision boundaries, predicted with the model, created a confusion matrix

--- DataScienceR/MachineLearning.R
+++ DataScienceR/MachineLearning.R
@@ -35,3 +35,36 @@ summary(model)
 #visualize the decision tree model
 plot(model)
 text(model)
+
+#load a color brewer library
+library(RColorBrewer)
+
+#create a color pallete
+palette <- brewer.pal(3, "Set2")
+
+#create a scatterplot colored by species
+plot(
+     x = iris$Petal.Length,
+     y = iris$Petal.Width,
+     pch = 19,
+     col = palette[as.numeric(iris$Species)],
+     main = "Iris Petal Length vs Width",
+     xlab = "Petal Length(cm)",
+     ylab = "Petal Width(cm)")
+
+#plot the decision boundaries
+partition.tree(
+               tree = model,
+               label = "Species",
+               add = TRUE) # meand we want to add these boundaries to our previous scatterplot
+
+#predict with the model
+predictions <- predict(
+                       object = model,
+                       newdata = test,
+                       type ="class")
+
+#create a confusion matrix
+table(
+      x = predictions,
+      y=test$Species)
